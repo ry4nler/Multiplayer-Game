55f78ad8a170d86f62f95c36c7081f683ca80590
ry4nler <ryanler@outlook.com>
2024-01-28 15:41:11 +0800

    added mutliplayer

--- client/sketch.js
+++ client/sketch.js
@@ -1,16 +1,94 @@
-const socket = io.connect("ws://localhost:8001");
-let em = new EntityManager();
-function setup() {
-	new Canvas("fullscreen");
+let ball;
+let em;
+let camManager;
+let currentRoomCode = null;
 
-	ball = new Sprite();
-	ball.diameter = 50;
+const socket = io.connect("ws://localhost:8001");
+
+window.onload = () => {
+    const join_option_input = prompt('Select: "CREATE" or "JOIN"', "CREATE");
+    if (join_option_input === "CREATE") {
+        socket.emit("requestCreateRoom");
+    } else if (join_option_input === "JOIN") {
+        const room_code_input = prompt("Enter Room Code");
+        socket.emit("requestJoinRoom", room_code_input);
+    } else {
+        window.onload();
+    }
+};
+
+socket.on("setRoomCode", (code) => {
+    currentRoomCode = code;
+});
+
+
+function setup() {
+    new Canvas("fullscreen");
+    em = new EntityManager();
+    camManager = new CameraManager(camera);
+    ball = createPlayerSprite();
+    camManager.setTarget(ball);
+    // p5play draws over our draw() loop, so we
+    // have to jump thru hoops to draw our text
+    // over our sprites...... by making a another
+    // sprite. wow.
+    let text_layer = new Sprite();
+    text_layer.visible = false;
+    text_layer.collider = "none";
+    text_layer.update = () => {
+        textAlign(CENTER, CENTER);
+        textSize(32);
+        text(`Room Code: ${currentRoomCode}`, 0, 50, width, 50);
+    };
 }
 
 function draw() {
-    background('grey');
+	if (!currentRoomCode) {
+        allSprites.visible = false;
+        push();
+        background("white");
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Room Not Found", 0, 0, width, height);
+        pop();
+        return;
+    }
+    background("grey");
     move();
-	socket.emit("position", ball.pos.x, ball.pos.y);
+    interpolateOtherPlayers();
+    camManager.update();
+    socket.emit("position", ball.pos.x, ball.pos.y);
+}
+
+function interpolateOtherPlayers() {
+    const now = +new Date();
+    const EXPECTED_SERVER_TICK_RATE = 20;
+    const est_render_timestamp = now - 1000.0 / EXPECTED_SERVER_TICK_RATE;
+    for (const [id, playerData] of em.entities) {
+        if (id == socket.id || playerData.positionBuffer.length < 2) {
+            continue;
+        }
+        while (
+            playerData.positionBuffer.length > 2 &&
+            playerData.positionBuffer[1].timestamp <= est_render_timestamp
+        ) {
+            playerData.positionBuffer.shift();
+        }
+        if (
+            playerData.positionBuffer.length >= 2 &&
+            playerData.positionBuffer[0].timestamp <= est_render_timestamp &&
+            est_render_timestamp <= playerData.positionBuffer[1].timestamp
+        ) {
+            const x0 = playerData.positionBuffer[0].x;
+            const x1 = playerData.positionBuffer[1].x;
+            const y0 = playerData.positionBuffer[0].y;
+            const y1 = playerData.positionBuffer[1].y;
+            const t0 = playerData.positionBuffer[0].timestamp;
+            const t1 = playerData.positionBuffer[1].timestamp;
+            playerData.sprite.x = x0 + (x1 - x0) * (est_render_timestamp - t0) / (t1 - t0);
+            playerData.sprite.y =  y0 + (y1 - y0) * (est_render_timestamp - t0) / (t1 - t0);
+        }
+    }
 }
 
 function move() {
@@ -28,6 +106,20 @@ function move() {
         ball.pos.x += SPEED;
     }
 }
+
+socket.on("buildMap", (mapData) => {
+    for (const blockData of Object.values(mapData.blocks)) {
+        let group = new Group();
+        Object.assign(group, blockData);
+        new Tiles(
+            blockData.tileMap,
+            blockData.startX,
+            blockData.startY,
+            blockData.w,
+            blockData.h
+        );
+    }
+});
 
 socket.on("playerDataUpdate", (id, playerData) => {
     for (let data of playerData) {
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import { readFileSync } from "fs";
 
 // create http server forwarding network traffic to socketio server
 const io = new Server(8001, {
@@ -6,44 +7,99 @@ const io = new Server(8001, {
         origin: "*",
     },
 });
+
 const clients = new Set();
+const rooms = []
 const TICK_DELAY = 1000 / 60;
+const MAPS_DATA = JSON.parse(readFileSync("./maps.json"));
 
 function Client(socket) {
     this.socket = socket;
     this.position = {x: 0, y: 0};
 }
 
-console.log("Server running...")
+class Room {
+    constructor(id) {
+        this.clients = [];
+        this.id = id;
+    }
 
-io.on("connection", socket => {
+    addClient(c) {
+        if (c.room) {
+            throw Error(`Client ${c.socket.id} already in room`);
+        }
+        this.clients.push(c);
+        c.room = this;
+    }
+
+    removeClient(c) {
+        if (!this.clients.includes(c)) {
+            throw Error(`No client ${c.socket.id} in room`);
+        }
+        this.clients.splice(this.clients.indexOf(c), 1);
+        if (this.clients.length === 0) {
+            rooms.splice(rooms.indexOf(this), 1);
+        }
+    }
+}
+
+console.log("Server running...");
+
+io.on("connection", (socket) => {
     console.log("New connection!");
     let client = new Client(socket);
     clients.add(client);
 
     socket.on("position", (x, y) => {
-        client.position = {x, y};
+        client.position = { x, y };
+    });
+
+    socket.on("requestCreateRoom", () => {
+        const roomCode = generateRandomRoomCode();
+        let room = new Room(roomCode);
+        room.addClient(client);
+        rooms.push(room);
+        socket.emit("setRoomCode", roomCode);
+        socket.emit("buildMap", MAPS_DATA.myWorld);
+    });
+
+    socket.on("requestJoinRoom", (code) => {
+        for (let room of rooms) {
+            if (room.id === code) {
+                room.addClient(client);
+                socket.emit("setRoomCode", code);
+                socket.emit("buildMap", MAPS_DATA.myWorld);
+                return;
+            }
+        }
     });
 
     socket.on("disconnect", () => {
+        if (client.room) {
+            client.room.removeClient(client);
+        }
         clients.delete(client);
-        clients.forEach(c => {
+        clients.forEach((c) => {
             c.socket.emit("removeClient", socket.id);
-        })
-    })
-})
+        });
+    });
+});
 
-
+function generateRandomRoomCode() {
+    return (+new Date()).toString(36).slice(-5);
+}
 
 function tick() {
-    let allData = [...clients].map(c => {
-        return {
-            position: c.position,
-            id: c.socket.id
+    for (let room of rooms) {
+        let allData = [...room.clients].map((c) => {
+            return {
+                position: c.position,
+                id: c.socket.id,
+            };
+        });
+        for (let c of room.clients) {
+            c.socket.emit("playerDataUpdate", c.socket.id, allData);
         }
-    });
-    for (let c of clients) {
-        c.socket.emit("playerDataUpdate", c.socket.id, allData);
     }
 }
 
